bbf71ed965a2f7f2608ee124cf0a39b2577635c0
Troy Haskin <git.troy@hask.in>
2014-01-02 15:57:56 -0600

    Finished toolbox path updater
    Compiles a path given a root and filters out desired folders from the
    path.

--- UpdateMatlabToolboxPath.m
+++ UpdateMatlabToolboxPath.m
@@ -1,25 +1,72 @@
-function [] = UpdateMatlabToolboxPath(ToolboxDirectory)
+function [] = UpdateMatlabToolboxPath(ToolboxDirectory,ExcludeTopDirectory)
     
-if (nargin ~= 1) || isempty(Path)
-    ToolboxDirectory = 'C:\Projects\MatlabToolbox';
+    if (nargin ~= 1) || isempty(ToolboxDirectory)
+        ToolboxDirectory = 'C:\Projects\MatlabToolbox';
+    end
+    
+    if (nargin ~= 2) || isempty(ExcludeTopDirectory)
+        ExcludeTopDirectory  = false;
+    end
+    
+    % Setup exclusions
+    DirectoriesToExclude = {'.git','Hash','Sandbox'};
+    DirectoriesToExclude = DirectoriesToExclude(:)' ;
+    
+    % Generate the full toolbox path
+    ToolboxPath = genpath(ToolboxDirectory);
+    
+    % Escape all metacharacters (since these are used in expressions)
+    DirectoriesToExclude = EscapeRegExMetacharacters(DirectoriesToExclude);
+    ToolboxDirectory     = EscapeRegExMetacharacters(ToolboxDirectory);
+    
+    % Create regular expression for filtering excluded directories
+    ExclusionRegEx= strcat(ToolboxDirectory,'\\'                    ,... % Top directory
+                           '(',strjoin(DirectoriesToExclude,'|'),')',... % Exclusion grouping
+                           '.*?;');                                      % Lazy wildcard grab
+    
+    % Remove all exluded directories
+    PathToAdd = regexprep(ToolboxPath,ExclusionRegEx,'');
+    
+    % Remove top level directory if requested
+    if ExcludeTopDirectory
+        PathToAdd = regexprep(PathToAdd,[ToolboxDirectory,';'],'');
+    end
+    
+    % Add  all included folders to the main search path
+    addpath(PathToAdd);
+    SaveOutcome = savepath();
+    
+    if (SaveOutcome == 0) 
+        fprintf(strrep(strrep(PathToAdd,'\','\\'),';',';\n'));
+        fprintf(['\n*** The above folders were successfully added and saved ',...
+                 'to the MATLAB search path.***\n']);
+    else
+        fprintf(['\n*** The Toolbox paths were UNSUCCESSFULLY saved ',...
+                 'to the MATLAB search path.\n***']);
+    end
+    
 end
 
-% Setup exclusions
-ExcludeTopDirectory  = false;
-DirectoriesToExclude = {'.git','Hash/JavaScript','Sandbox'};
-DirectoriesToExclude = DirectoriesToExclude(:);
-
-% Generate the full toolbox path
-ToolboxPath = genpath(ToolboxDirectory);
-
-% Escape all backslashes
-ToolboxPath               = strrep(ToolboxPath     ,'\','\\');
-ToolboxDirectory          = strrep(ToolboxDirectory,'\','\\');
-
-% Create regular expression for filtering excluded directories
-ExclusionRegularExpression = strcat(ToolboxDirectory,'\\',...
-                                    DirectoriesToExclude,'(\\ | ;)');
-
-fprintf(strrep(strrep(ToolboxPath,'\','\\'),';',';\n'));
-
+function EscapedString = EscapeRegExMetacharacters(String)
+    
+    EscapedString = String                          ;
+    Escape        = @(c,Str) strrep(Str,c,['\',c])  ;
+    
+    EscapedString = Escape('\', EscapedString);
+    EscapedString = Escape('^', EscapedString);
+    EscapedString = Escape('$', EscapedString);
+    EscapedString = Escape('.', EscapedString);
+    EscapedString = Escape('|', EscapedString);
+    EscapedString = Escape('?', EscapedString);
+    EscapedString = Escape('*', EscapedString);
+    EscapedString = Escape('+', EscapedString);
+    EscapedString = Escape('-', EscapedString);
+    EscapedString = Escape(':', EscapedString);
+    EscapedString = Escape('(', EscapedString);
+    EscapedString = Escape(')', EscapedString);
+    EscapedString = Escape('[', EscapedString);
+    EscapedString = Escape(']', EscapedString);
+    EscapedString = Escape('{', EscapedString);
+    EscapedString = Escape('}', EscapedString);
+    
 end
