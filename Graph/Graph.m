454a8298bd6eab6799cb27683e004a0ca0a0d83c
Troy Haskin <git.troy@hask.in>
2014-05-12 18:54:32 -0500

    Graph Class development
    Added private Node and Edge counts.
    
    The constructor now generates arrays as the values of the Node and Edge
    properties.
    
    Starting developing a key-value constructor system.

--- Graph/Graph.m
+++ Graph/Graph.m
@@ -5,16 +5,36 @@ classdef Graph < handle % GraphAbstract
         Edges
     end
     
+    properties( Hidden = true , Access = private )
+        NodeCount = 0;
+        EdgeCount = 0;
+        
+        ConstructorKeyPropertyMap = struct(...
+            'NodeNames','Name','IDs')
+    end
+    
     methods
 
         function G = Graph(Nnodes,Nedges)
             
+            %   Allocate the requested number of nodes
             if (nargin >= 1) && isnumeric(Nnodes) && isscalar(Nnodes)
-                G.Nodes = Node(Nnodes);
+                G.Nodes = Node();
+                G.Nodes.Name        = repmat({''},Nnodes);
+                G.Nodes.ID          = zeros(Nnodes,1);
+                G.Nodes.Value       = cell(Nnodes,1);
+                G.Nodes.NeighborIDs = repmat({[]},Nnodes,1);
+                G.Nodes.EdgeIDs     = repmat({[]},Nnodes,1);
             end
             
+            %   Allocate the requested number of edges
             if (nargin >= 2) && isnumeric(Nedges) && isscalar(Nedges)
-                G.Edges = Edge(Nedges);
+                G.Edges = Edge();
+                G.Edges.Name        = repmat({''},Nedges);
+                G.Edges.ID          = zeros(Nedges,1);
+                G.Edges.Value       = cell(Nedges,1);
+                G.Edges.NeighborIDs = cell(Nedges,1);
+                G.Edges.EdgeIDs     = cell(Nedges,1);
             end
             
         end
