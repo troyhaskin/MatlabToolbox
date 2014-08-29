d7f9beefb67feb1dc7aeb82bce90de152c43209d
Troy Haskin <git.troy@hask.in>
2014-08-28 20:56:17 -0500

    Added blocking, string casting, 8-bit assumption

--- Sandbox/SHA256.m
+++ Sandbox/SHA256.m
@@ -1,5 +1,5 @@
 function Hash = SHA256(Message)
-
+    
     % Helper functions
     R   = @(v,n) bitshift(v,-n);
     S   = @(v,n) bitor(bitshift(v,-n),bitshift(v,(32-n)));
@@ -11,137 +11,100 @@ function Hash = SHA256(Message)
     sigma1 = @(x)  bitxor( S(x,17), bitxor( S(x,19) , R(x,10) ));
     Mod32Add = @(x) uint32(mod(sum(uint64(x)),2^32));
     
-    % Initial hash vector
+    % Initial hash vector and constants
     [H,K] = LoadSHA256InitialVectorAndConstants();
-    a = H(1);
-    b = H(2); 
-    c = H(3); 
-    d = H(4); 
-    e = H(5); 
-    f = H(6); 
-    g = H(7); 
-    h = H(8); 
     
-    % Count the number of bits in Message
-    M       = uint8(Message);
-    l       = GetMessageBitSize(M);
+    % Cast to a string if needed
+    if isnumeric(Message);
+        Message = num2str(Message);
+    end
+    
+    % Count the number of bits in Message assuming an 8-bit encoding
+    M = dec2bin(Message,8);
+    l = numel(M);
     
     % Determine the length of the zero padding
-    nZeros = 448 - mod(l+1,512);
+    nZeros = max(mod(448 - (l+1),512),0);
     
     % Create the padded Message
-    M = [reshape(dec2bin(M,8)',1,l),'1',repmat('0',1,nZeros),dec2bin(l,64)];
-    M = uint32(bin2dec(reshape(M,32,16)'));
+    Pad =['1',repmat('0',1,nZeros),dec2bin(l,64)];
+    M = [reshape(M',1,l),Pad];
     
-    % Allocate expanded Message blocks
+    
+    %   Split message into a vector of 32-bit words; 
+    %   every 16 words comprises one 512-bit message block.
+    nWords  = length(M)/32;
+    nBlocks = nWords/16;
+    iBlock  = 1:16;
+    M = uint32(bin2dec(reshape(M,32,nWords)'));
+    
+    % Allocate expanded Message block array
     W = ones(64,1,'uint32');
-    W(1:16) = M;
     
     % Perform the compression for the first 16 iterations
-    for j = 1:16
-        T1 = Mod32Add([h,SIGMA1(e),Ch(e,f,g),K(j),W(j)]);
-        T2 = Mod32Add([SIGMA0(a),Maj(a,b,c)]);
-        h = g;
-        g = f;
-        f = e;
-        e = Mod32Add([d,T1]);
-        d = c;
-        c = b;
-        b = a;
-        a = Mod32Add([T1,T2]);
+    for k = 1:nBlocks
+        % Assign intermediate hash values to registers
+        a = H(1);   b = H(2);   c = H(3);   d = H(4);
+        e = H(5);   f = H(6);   g = H(7);   h = H(8);
+        
+        % Assign current message block to expanded message block vector
+        W(1:16) = M(iBlock + 16*(k-1));
+        
+        % Compression function: first 16 iterations
+        for j = 1:16
+            T1 = Mod32Add([h,SIGMA1(e),Ch(e,f,g),K(j),W(j)]);
+            T2 = Mod32Add([SIGMA0(a),Maj(a,b,c)]);
+            h = g;
+            g = f;
+            f = e;
+            e = Mod32Add([d,T1]);
+            d = c;
+            c = b;
+            b = a;
+            a = Mod32Add([T1,T2]);
+        end
+        
+        % Perform the last iterations, now Wj needs to be calculate
+        for j = 17:64
+            W(j) = Mod32Add([sigma1(W(j-2)),W(j-7),sigma0(W(j-15)),W(j-16)]) ;
+            T1 = Mod32Add([h,SIGMA1(e),Ch(e,f,g),K(j),W(j)]);
+            T2 = Mod32Add([SIGMA0(a),Maj(a,b,c)]);
+            h = g;
+            g = f;
+            f = e;
+            e = Mod32Add([d,T1]);
+            d = c;
+            c = b;
+            b = a;
+            a = Mod32Add([T1,T2]);
+        end
+        
+        % Update intermediate hash value
+        H = Mod32Add([[a;b;c;d;e;f;g;h],H]')';
     end
     
-    % Perform the last iterations, now Wj needs to be calculate
-    for j = 17:64
-        W(j) = Mod32Add([sigma1(W(j-2)),W(j-7),sigma0(W(j-15)),W(j-16)]) ;
-        T1 = Mod32Add([h,SIGMA1(e),Ch(e,f,g),K(j),W(j)]);
-        T2 = Mod32Add([SIGMA0(a),Maj(a,b,c)]);
-        h = g;
-        g = f;
-        f = e;
-        e = Mod32Add([d,T1]);
-        d = c;
-        c = b;
-        b = a;
-        a = Mod32Add([T1,T2]);
-    end
-    
-    Hash = Mod32Add([[a;b;c;d;e;f;g;h],H]');
+    % Convert intermediate hash value to the final output
+    Hash = lower(reshape(dec2hex(H)',1,64));
     
 end
 
-function Size = GetMessageBitSize(Message) %#ok<INUSD>
-    Info = whos('Message');
-    Size = (Info.bytes)*8;
-end
 
 function [H,K] = LoadSHA256InitialVectorAndConstants()
-    H = uint32(hex2dec(['6a09e667';'bb67ae85';'3c6ef372';'a54ff53a';...
-            '510e527f';'9b05688c';'1f83d9ab';'5be0cd19']));
-    K = uint32(hex2dec(['428a2f98';
-        '71374491';
-        'b5c0fbcf';
-        'e9b5dba5';
-        '3956c25b';
-        '59f111f1';
-        '923f82a4';
-        'ab1c5ed5';
-        'd807aa98';
-        '12835b01';
-        '243185be';
-        '550c7dc3';
-        '72be5d74';
-        '80deb1fe';
-        '9bdc06a7';
-        'c19bf174';
-        'e49b69c1';
-        'efbe4786';
-        '0fc19dc6';
-        '240ca1cc';
-        '2de92c6f';
-        '4a7484aa';
-        '5cb0a9dc';
-        '76f988da';
-        '983e5152';
-        'a831c66d';
-        'b00327c8';
-        'bf597fc7';
-        'c6e00bf3';
-        'd5a79147';
-        '06ca6351';
-        '14292967';
-        '27b70a85';
-        '2e1b2138';
-        '4d2c6dfc';
-        '53380d13';
-        '650a7354';
-        '766a0abb';
-        '81c2c92e';
-        '92722c85';
-        'a2bfe8a1';
-        'a81a664b';
-        'c24b8b70';
-        'c76c51a3';
-        'd192e819';
-        'd6990624';
-        'f40e3585';
-        '106aa070';
-        '19a4c116';
-        '1e376c08';
-        '2748774c';
-        '34b0bcb5';
-        '391c0cb3';
-        '4ed8aa4a';
-        '5b9cca4f';
-        '682e6ff3';
-        '748f82ee';
-        '78a5636f';
-        '84c87814';
-        '8cc70208';
-        '90befffa';
-        'a4506ceb';
-        'bef9a3f7';
-        'c67178f2']));
+    H = uint32(hex2dec([...
+        '6a09e667';'bb67ae85';'3c6ef372';'a54ff53a';...
+        '510e527f';'9b05688c';'1f83d9ab';'5be0cd19']));
+    K = uint32(hex2dec([...
+        '428a2f98';'71374491';'b5c0fbcf';'e9b5dba5';'3956c25b';'59f111f1';
+        '923f82a4';'ab1c5ed5';'d807aa98';'12835b01';'243185be';'550c7dc3';
+        '72be5d74';'80deb1fe';'9bdc06a7';'c19bf174';'e49b69c1';'efbe4786';
+        '0fc19dc6';'240ca1cc';'2de92c6f';'4a7484aa';'5cb0a9dc';'76f988da';
+        '983e5152';'a831c66d';'b00327c8';'bf597fc7';'c6e00bf3';'d5a79147';
+        '06ca6351';'14292967';'27b70a85';'2e1b2138';'4d2c6dfc';'53380d13';
+        '650a7354';'766a0abb';'81c2c92e';'92722c85';'a2bfe8a1';'a81a664b';
+        'c24b8b70';'c76c51a3';'d192e819';'d6990624';'f40e3585';'106aa070';
+        '19a4c116';'1e376c08';'2748774c';'34b0bcb5';'391c0cb3';'4ed8aa4a';
+        '5b9cca4f';'682e6ff3';'748f82ee';'78a5636f';'84c87814';'8cc70208';
+        '90befffa';'a4506ceb';'bef9a3f7';'c67178f2']));
 end
 
 
