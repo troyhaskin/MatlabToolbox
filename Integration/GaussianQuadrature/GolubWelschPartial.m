abb5701d9ced8a0b6fef296d39c65b8e087dfe06
Troy Haskin <git.troy@hask.in>
2015-03-12 16:23:11 -0500

    Correct comments; moved full() to eig() call

--- Integration/GaussianQuadrature/GolubWelschPartial.m
+++ Integration/GaussianQuadrature/GolubWelschPartial.m
@@ -1,20 +1,22 @@
 function xk = GolubWelschPartial(ak,bk,ck)
     %GolubWelschPartial
-    %   Calculate the approximate* nodes and weights (normalized to 1) of an orthogonal 
-    %   polynomial family defined by a three-term reccurence relation of the form
+    %   Calculate the nodes of an orthogonal polynomial family defined by a three-term 
+    %   reccurence relation of the form:
     %       x pk(x) = ak pkp1(x) + bk pk(x) + ck pkm1(x)
     %   
-    %   The weight scale factor mu0 is the integral of the weight function over the
-    %   orthogonal domain.
+    %   The weights via the first eigenvector are not calculated for the partial implementation.
+    %   This function should be used when an explicit forumla for the weights is needed since
+    %   calculation of the eigenvectors does not scale linearly with the order of the recurrence
+    %   relation.
     %
     
     %   Calculate the terms for the orthonormal version of the polynomials
     alpha = sqrt(ak(1:end-1) .* ck(2:end));
     
     %   Build the symmetric tridiagonal matrix
-    T = full(spdiags([[alpha;0],bk,[0;alpha]],[-1,0,+1],length(alpha),length(alpha)));
+    T = spdiags([[alpha;0],bk,[0;alpha]],[-1,0,+1],length(alpha),length(alpha));
     
     %   Calculate the eigenvectors and values of the matrix
-    xk = eig(T,'vector');
+    xk = eig(full(T),'vector');
     
 end
